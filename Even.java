bc3af72a39c75f396fa7e487210c1619a5ed8f84
Ramkumar3197 <Ramkumar3197@users.noreply.github.com>
2017-06-05 03:36:55 -0700

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Even
+{
+public static void main(String args[])
+{
+Scanner b=new Scanner(System.in);
+int n;
+n=b.nextInt();
+if(n%2==0)
+{
+System.out.print("Even");
+}
+else
+System.out.print("Odd");
+}
+}
+
