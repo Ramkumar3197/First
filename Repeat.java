d0236f75d1c0c77085792482520d916f288e6190
Ramkumar3197 <Ramkumar3197@users.noreply.github.com>
2017-06-07 05:35:50 -0700

    Create Repeat.java

---
+++ Repeat.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+class Repeat
+{
+public class void main(String args[])
+{
+Scanner f=new Scanner(System.in);
+int n,j,k;
+for(j=0;j<args.length;j++)
+{
+a[j]=f.nextInt();
+}
+for(i=0;i<args.length;i++)
+{
+for(k=i+1;k<args.length;k++)
+{
+if(a[j]==a[k])
+{
+System.out.println(a[j]);
+i=args.length;
+}
+else
+{
+i++;
+}
+}
+}
+}
+}
