7ed06bd0e4141114bc44a2ea1e054144d217a390
Ramkumar3197 <Ramkumar3197@users.noreply.github.com>
2017-06-07 04:59:03 -0700

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.*;
+class Index1
+{
+void check(int[] m)
+{
+int j,l;
+System.out.println("");
+for(j=0,l=0;j<m.length,l.m<length;j++,l++)
+{
+if(j==a[l])
+{
+System.out.print("The number "+a[l]+" equals its index "+j);
+}
+}
+}
+}
+class Index
+{
+public static void main(String args[])
+{
+Scanner b=new Scanner(System.in);
+int n,i,j;
+int[] a=new int[n];
+n=s.nextInt();
+for(i=0;i<n;i++)
+{
+a[i]=a.nextInt();
+}
+Arrays.sort(a);
+Index1 k=new Index();
+k.check(a);
+}
