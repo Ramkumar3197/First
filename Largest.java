1d3ca87942da175d319ee9fef009b9c011e4a0e4
Ramkumar3197 <Ramkumar3197@users.noreply.github.com>
2017-06-07 02:49:40 -0700

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Largest
+{
+public static void main(String args[])
+{
+Scanner x=new Scanner(System.in);
+int[] a=new int[3];
+int i;
+for(i=0;i<3;i++)
+{
+a[i]=s.nextInt();
+}
+int d;
+d=Math.max(a[0],a[1],a[2]);
+System.out.println(d);
+}
+}
